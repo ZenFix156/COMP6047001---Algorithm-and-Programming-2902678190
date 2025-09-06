f2ffe7e3979948bce327fc02575aa254b9273fa5
ZenFix156 <95063451+ZenFix156@users.noreply.github.com>
2025-09-06 15:42:39 +0700

    Update Favorite Programming Language.c

--- Favorite Programming Language.c
+++ Favorite Programming Language.c
@@ -2,10 +2,11 @@
 
 int main() {
     char name[] = "Marshall Razzan";
-    char favorite_languages[] = "Python ,C & Java";
+    char favorite_languages[] = "Python & C";
 
     printf("My name is %s\n", name);
     printf("My favorite programming languages are %s\n", favorite_languages);
     
     return 0;
+
 }
