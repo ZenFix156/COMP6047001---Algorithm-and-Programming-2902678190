be76b804271dd6fedc75e56ba0d0e145924c1957
ZenFix156 <95063451+ZenFix156@users.noreply.github.com>
2025-09-06 15:41:53 +0700

    Add files via upload

---
+++ Favorite Programming Language.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Marshall Razzan";
+    char favorite_languages[] = "Python ,C & Java";
+
+    printf("My name is %s\n", name);
+    printf("My favorite programming languages are %s\n", favorite_languages);
+    
+    return 0;
+}
